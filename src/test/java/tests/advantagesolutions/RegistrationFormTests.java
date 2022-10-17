06755183d3f91bf55cba5eeb9cb8844d048bc077
Viktor_Rus <vaurusov@mts.ru>
2022-10-17 17:19:55 +0300

    first commit

---
+++ src/test/java/tests/advantagesolutions/RegistrationFormTests.java
@@ -0,0 +1,23 @@
+package tests.demoqa;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
+
+public class RegistrationFormTests extends TestBase {
+    @Test
+    void fillFormTest() {
+        step("Open registrations form", () -> {
+            open("/automation-practice-form");
+            $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+            executeJavaScript("$('footer').remove()");
+            executeJavaScript("$('#fixedban').remove()");
+        });
+//
+
+    }
+}
