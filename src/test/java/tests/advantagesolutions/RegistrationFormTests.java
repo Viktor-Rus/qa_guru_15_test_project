cbcd6b6ba9400166b7ac8e5aab21e01717a1881e
Viktor_Rus <vaurusov@mts.ru>
2022-10-18 15:55:39 +0300

    test form

--- src/test/java/tests/advantagesolutions/RegistrationFormTests.java
+++ src/test/java/tests/advantagesolutions/RegistrationFormTests.java
@@ -1,21 +1,19 @@
-package tests.demoqa;
+package tests.advantagesolutions;
+
 
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.appear;
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
 public class RegistrationFormTests extends TestBase {
     @Test
-    void fillFormTest() {
+    void mainLogoVisible() {
         step("Open registrations form", () -> {
-            open("/automation-practice-form");
-            $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
-            executeJavaScript("$('footer').remove()");
-            executeJavaScript("$('#fixedban').remove()");
+            open("/");
+            $("svg[class*='main-logo']").should(appear);
+
         });
 //
 
--- src/test/java/tests/advantagesolutions/TestBase.java
+++ src/test/java/tests/advantagesolutions/TestBase.java
@@ -1,10 +1,8 @@
-package tests.demoqa;
+package tests.advantagesolutions;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
-import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
