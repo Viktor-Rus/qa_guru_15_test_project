9baa7251c53b058bc937c61d10e50ecca8015f3a
Viktor_Rus <vaurusov@mts.ru>
2022-10-21 17:04:05 +0300

    edit Configuration

--- src/test/java/tests/advantagesolutions/TestBase.java
+++ src/test/java/tests/advantagesolutions/TestBase.java
@@ -22,8 +22,10 @@ public class TestBase {
 
         Configuration.browserCapabilities = capabilities;
         Configuration.baseUrl = "https://advantagesolutions.net/";
-        Configuration.browserSize = "1920x1080";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+//        Configuration.browserSize = "1920x1080";
+        Configuration.browserSize = System.getProperty("browser_size");
+        Configuration.remote = System.getProperty("remote_selenide");
+//        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 
     @AfterEach
